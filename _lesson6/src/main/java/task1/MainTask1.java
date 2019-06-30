7f45ebf49ca9769c16f83f1a44eba6620d8a46df
Zendux <zendux.bb@gmail.com>
2019-06-30 07:30:28 +0300

    &!&4243

---
+++ _lesson6/src/main/java/task1/MainTask1.java
@@ -0,0 +1,15 @@
+package task1;
+
+import java.util.Arrays;
+
+public class MainTask1 {
+
+    public static int[] arrayTransform (int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == 4) {
+                return Arrays.copyOfRange(array, i + 1, array.length);
+            }
+        }
+        throw new RuntimeException("Неверный массив");
+    }
+}
---
+++ _lesson6/src/main/java/task3/DAO/DAO.java
@@ -0,0 +1,19 @@
+package task3.DAO;
+import task3.model.Students;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DAO {
+    void addStudent(String surname, int score) throws SQLException;
+
+    void updateStudentScoreById(int id, int score) throws SQLException;
+
+    void deleteStudentById(int id) throws SQLException;
+
+    List<Students> getStudentById(int id) throws SQLException;
+
+    List<Students> getStudentsBetweenScore(int scoreStart,int scoreEnd) throws SQLException;
+
+    List<Students> getAllStudents() throws SQLException;
+}
